74aa37618b5eb46ce4a68fd0b19c37dad4eb5c1e
plkujaw <jawor.jakub@gmail.com>
2020-07-08 16:43:28 +0100

    next-previous buttons approach, updated css

--- app.js
+++ app.js
@@ -20,15 +20,30 @@ prevBtn.addEventListener("click", () => {
 
 carousel = () => {
   // working with slides
-  // when we reach last slide, go back to the first one
-  if (counter === slides.length) {
-    counter = 0;
+  // // when we reach last slide, go back to the first one
+  // if (counter === slides.length) {
+  //   counter = 0;
+  // }
+  // // when we are at first one and click previous, go to the last one
+  // if (counter < 0) {
+  //   counter = slides.length - 1;
+  // }
+
+  // working with buttons
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = "block";
+  } else {
+    nextBtn.style.display = "none";
   }
-  // when we are at first one and click previous, go to the last one
-  if (counter < 0) {
-    counter = slides.length - 1;
+  
+  if (counter > 0) {
+    prevBtn.style.display = "block";
+  } else {
+    prevBtn.style.display = "none";
   }
+
   slides.forEach(function(slide) {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 }
+prevBtn.style.display = "none";
