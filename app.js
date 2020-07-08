f12987d8b0e4e2269389e22abe9f5022abf97b46
plkujaw <jawor.jakub@gmail.com>
2020-07-08 15:47:34 +0100

    js setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+slides.forEach(function(slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+carousel = () => {
+  slides.forEach(function(slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`
+  });
+}
+
+nextBtn.addEventListener("click", () => {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  counter--;
+  carousel();
+});
